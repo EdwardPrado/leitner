3034fa27b1e538399bc08f828f18191cd31ecd09
Ed Prado <edward.a.prado@gmail.com>
2026-01-09 01:11:09 -0500

    feat: toolbar popup

---
+++ src/popup.js
@@ -0,0 +1,44 @@
+const TIMEOUT = 2000
+const form = document.getElementById("leitnerForm")
+
+form.addEventListener("submit", async (event) => {
+  event.preventDefault()
+
+  if (!form.checkValidity()) {
+    form.reportValidity()
+    return
+  }
+
+  const inputName = document.getElementById("libraryName")
+  const inputLink = document.getElementById("libraryLink")
+
+  await browser.storage.local.set({
+    libraryInfo: {
+      name: inputName.value,
+      link: inputLink.value,
+    },
+  })
+
+  const saveButton = document.getElementById("save-btn")
+
+  saveButton.value = "Saved"
+  saveButton.classList.add("saved")
+
+  setTimeout(() => {
+    saveButton.value = "Save"
+    saveButton.classList.remove("saved")
+  }, TIMEOUT)
+})
+
+async function popup() {
+  const libraryInfo = await browser.storage.local
+    .get("libraryInfo")
+    .then((res) => res.libraryInfo)
+
+  if (libraryInfo) {
+    document.getElementById("libraryName").value = libraryInfo.name
+    document.getElementById("libraryLink").value = libraryInfo.link
+  }
+}
+
+popup()
